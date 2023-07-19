3f4e747659adc752c452390f9d2f457320b762b4
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2023-07-19 00:37:24 -0700

    Initial Commit

---
+++ Number Converter/Number_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_ConverterApp.swift
+//  Number Converter
+//
+//  Created by Dylan Conklin on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Number_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
